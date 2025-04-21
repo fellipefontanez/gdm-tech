643eccd184a02bf95a1743e5fefb3cfb9cb7f128
Fellipe Fontanez <fellipefontanez@gmail.com>
2025-04-21 14:46:41 -0300

    FeatureCards

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import FeatureCards from "@/src/components/FeatureCards/cards";
 import HeroBanner from "@/src/components/HeroBanner/banner";
 
 export default function Home() {
   return (
     <section>
       <HeroBanner />
+      <FeatureCards />
     </section>
   );
 }
