51f59530c7c0f254226ae6280ef4b06df07aa38b
Fellipe Fontanez <fellipefontanez@gmail.com>
2025-04-21 23:13:03 -0300

    voltando serverside

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,38 +1,22 @@
-"use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import GamesList from "@/src/components/GamesList/lista";
 import { getAdvantages } from "./util/fetchs";
 import BarraDeFavoritos from "@/src/components/FavoritosBar/Barra";
 import { Loader } from "@/src/components/Loader/Loader";
-import { AdvantagesV1Response } from "@/src/types/eventResponse.model";
 
-const Home = () => {
-  const [response, setResponse] = useState<AdvantagesV1Response>();
-  const [loading, setLoading] = useState(true);
+const Home = async () => {
+  const response = await getAdvantages();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getAdvantages();
-        setResponse(data);
-      } catch (error) {
-        console.error("Erro ao carregar os dados:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) return <Loader />;
+  if (!response) return <Loader />;
 
   return (
     <main className="pt-[50px] md:p-0">
       <BarraDeFavoritos />
       <section className="grid md:grid-cols-12">
         <div className="hidden md:block md:col-span-3"></div>
-        <div className="col-span-12 md:col-span-6 md:col-start-4 py-8">{response && <GamesList data={response} />}</div>
+        <div className="col-span-12 md:col-span-6 md:col-start-4 py-8">
+          <GamesList data={response} />
+        </div>
       </section>
     </main>
   );
